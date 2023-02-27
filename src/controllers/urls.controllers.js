1eb0c10e5e9bbe15e11c0797d921db7c8b4ecea8
expeditus303 <ricardotonietto@gmail.com>
2023-02-27 20:54:11 -0300

    feat: open short url

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -1,24 +1,39 @@
 import { nanoid } from "nanoid";
+import { connectiondb } from "../database/database.connection.js";
 import { createShortUrl } from "../repository/urls.repositories.js";
 
 export async function shortenUrl(req, res) {
   const { url } = req.body;
 
-  const userId = res.locals.userId
+  const userId = res.locals.userId;
 
   const shortUrl = nanoid(10);
 
   try {
-    const shortUrlId = await createShortUrl(userId, shortUrl, url)
+    const shortUrlId = await createShortUrl(userId, shortUrl, url);
 
-    const id = shortUrlId.rows[0].id
+    const id = shortUrlId.rows[0].id;
 
     const sendShortUrl = {
-        id,
-        shortUrl
-    }
+      id,
+      shortUrl,
+    };
+    res.status(201).send(sendShortUrl);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
 
-    res.status(201).send(sendShortUrl)
+export async function openUrl(req, res) {
+  const { shortUrl } = req.params;
+
+  try {
+    const url = await connectiondb.query(`UPDATE urls SET "visitCount" = "visitCount" + 1 WHERE "shortUrl" = $1 RETURNING url`, [shortUrl])
+
+    if (url.rowCount === 0) return res.sendStatus(404)
+
+    const openUrl = url.rows[0].url
+    res.redirect(openUrl)
   } catch (err) {
     res.status(500).send(err.message);
   }
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { shortenUrl } from "../controllers/urls.controllers.js";
+import { openUrl, shortenUrl } from "../controllers/urls.controllers.js";
 import { tokenValidation } from "../middlewares/auths.middlewares.js";
 import { validateModelBody } from "../middlewares/validateModel.middleware.js";
 import { urlModel } from "../models/urls.models.js";
 
 const urlRoutes = Router();
 
+urlRoutes.get("/urls/open/:shortUrl", openUrl)
 urlRoutes.post("/urls/shorten", tokenValidation, validateModelBody(urlModel), shortenUrl);
 
 export default urlRoutes;
