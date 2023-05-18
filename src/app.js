463b6ac69ed9e5f758b3c7135446dc7c8382432f
expeditus303 <ricardotonietto@gmail.com>
2023-05-18 13:50:55 -0300

    feat: errors middleware

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import express from "express"
 import cors from "cors"
 import "dotenv/config"
 import routes from "./routes/app.routes.js";
-// import errorsMiddleware from "./middlewares/errors.middleware.js";
+import errorsMiddleware from "./middlewares/errors.middleware.js";
+
 
 const app = express()
 
@@ -11,7 +12,7 @@ app.use(cors())
 
 app.use(routes)
 
-// app.use(errorsMiddleware)
+app.use(errorsMiddleware)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
---
+++ src/middlewares/errors.middleware.js
@@ -0,0 +1,14 @@
+import { StatusCodes } from "http-status-codes";
+
+const errorsStatusCode = {
+    UnprocessableEntityError: StatusCodes.UNPROCESSABLE_ENTITY
+}
+
+function errorsMiddleware(err, req, res, next){
+    const errorMessage = err.message || "Internal Server Error"
+    const errorStatusCode = errorsStatusCode[err.name] || StatusCodes.INTERNAL_SERVER_ERROR
+
+    res.status(errorStatusCode).send(errorMessage)
+}
+
+export default errorsMiddleware
